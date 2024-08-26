0ad347df249f2b7275ade01e0cd98498eecef051
Marley Reeves <marley@marlstar.com>
2024-08-26 16:49:20 +1000

    configure lualine, screenkey

--- lua/plugins/screenkey.lua
+++ lua/plugins/screenkey.lua
@@ -1,4 +1,15 @@
 return {
     "NStefan002/screenkey.nvim",
     lazy = false,
+	config = function ()
+		local sk = require("screenkey")
+		sk.setup({
+			win_opts = {
+				height = 1,
+				width = 30,
+				border = "rounded",
+			}
+		})
+		if not sk.is_active() then vim.cmd("Screenkey toggle") end
+	end
 }
