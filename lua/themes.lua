916db28e332bc86d37780671f2b77c1d6abe1fb6
Marley Reeves <marley@marlstar.com>
2024-08-27 10:03:47 +1000

    add more themes

---
+++ lua/themes.lua
@@ -0,0 +1,12 @@
+return {
+	require("plugins.themery"), -- Theme selector
+
+	require("themes.onedark"),
+	require("themes.catppuccin"),
+	require("themes.tokyonight"),
+	require("themes.flow"),
+	require("themes.tokyodark"),
+	require("themes.kanagawa"),
+	require("themes.cyberdream"),
+	require("themes.moonlight"),
+}
