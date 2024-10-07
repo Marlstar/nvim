731e29935f6744280d269897ea52c6e01e9967ea
Marley Reeves <marley@marlstar.com>
2024-10-08 08:18:24 +1000

    neovide background image option for mysty0 fork

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,3 +1,7 @@
 vim.g.neovide_cursor_animate_command_line = false
 vim.g.neovide_cursor_animation_length = 0.08
 vim.g.neovide_cursor_trail_size = 0.5
+
+-- Will only work using mysty0's fork of neovide
+vim.g.neovide_background_image = "C:/users/marle/Pictures/neovide_background.jpg"
+vim.g.neovide_background_transparency = 0.5
