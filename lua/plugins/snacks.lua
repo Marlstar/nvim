b915bbfa36bb953646f2983c16fd196e681542ce
Marley Reeves <marley@marlstar.com>
2025-01-15 17:02:19 +1000

    snacks.nvim - dashboard setup

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,5 +4,25 @@ return {
     lazy = false,
     dependencies = "nvim-tree/nvim-web-devicons",
     ---@type snacks.Config
-    opts = {}
+    opts = {
+		-- Home dashboard
+		dashboard = {
+			sections = {
+				{
+					section = "terminal",
+					cmd = "cat " .. vim.fn.stdpath("config") .. "/resources/dash.txt",
+					height = 17,
+					padding = 1,
+					-- remove the image if it takes up too much space
+					enabled = function () return vim.api.nvim_win_get_height(0) >= 35 end
+				},
+				{ section = "keys", gap = 1, padding = 1 },
+				{ section = "startup" },
+			},
+		},
+	},
+
+	keys = {
+		{ "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete buffer" },
+	}
 }
