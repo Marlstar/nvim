87b1327c384bbf9f1df148c5c95c5b074aae9621
Marley Reeves <marley@marlstar.com>
2025-01-17 20:26:33 +1000

    Neo-Tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,16 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	cmd = "Neotree",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	keys = {
+		{ "<leader>tt", function() require("neo-tree.command").execute{ toggle=true } end, desc = "Neotree - standard" },
+		{ "<leader>tf", function() require("neo-tree.command").execute{ position="float", toggle=true} end, desc = "Neotree - float"},
+		{ "<leader>tg", function() require("neo-tree.command").execute{ source="git_status", toggle=true } end, desc = "Neotree - git status"},
+		{ "<leader>tx", function() require("neo-tree.command").execute{ close=true } end, desc = "Neotree - close"},
+	}
+}
