e71a53332298ce0e9eae355f640973ffdb4703d6
Marley Reeves <marley@marlstar.com>
2025-01-17 14:38:33 +1000

    bufferline config

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,27 @@
+return {
+	"akinsho/bufferline.nvim",
+	version = "*",
+	lazy = false,
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		require("bufferline").setup({
+			options = {
+				mode = "buffers",
+				themable = false, -- use colourscheme colours
+				separator_style = "slant",
+				numbers = "none",
+				max_name_length = 25,
+				indicator = {
+					style = "icon"
+				},
+			}
+		})
+	end,
+
+	keys = {
+		{ "<leader>bn", ":BufferLineCycleNext<CR>", desc = "Next tab" },
+		{ "<leader>bp", ":BufferLineCyclePrev<CR>", desc = "Previous tab" },
+		{ "<leader>bmn", ":BufferLineMoveNext<CR>", desc = "Move tab right" },
+		{ "<leader>bmp", ":BufferLineMovePrev<CR>", desc = "Move tab left" },
+	}
+}
