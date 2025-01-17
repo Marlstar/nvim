2790a932aa8551a2fe8ce6643a71925b61c634b8
Marley Reeves <marley@marlstar.com>
2025-01-17 18:40:58 +1000

    load Snacks library for luals

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -4,6 +4,7 @@ return {
 		ft = "lua",
 		opts = {
 			library = {
+				{ path = vim.fn.stdpath("data") .. "/lazy/snacks.nvim", words = { "Snacks" } },
 				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 			}
 		}
