d0c3fb6555e494613892236e0d3e3326fb6e6885
Marley Reeves <marley@marlstar.com>
2025-01-17 17:23:52 +1000

    telescope.nvim

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,31 @@
+return {
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		cmd = { "Telescope" },
+		opts = {
+			mappings = {
+				n = {
+					["q"] = require("telescope.actions").close
+				}
+			}
+		},
+		keys = {
+			{ "<leader>ff", require("telescope.builtin").find_files },
+			{ "<leader>fg", require("telescope.builtin").live_grep },
+			{ "<leader>fz", require("telescope.builtin").current_buffer_fuzzy_find },
+			{ "<leader>fd", require("telescope.builtin").diagnostics },
+			{ "<leader>fb", require("telescope.builtin").buffers },
+			{ "<leader>fh", require("telescope.builtin").highlights },
+			{ "<leader>fm", require("telescope.builtin").marks },
+		},
+		init = function()
+			vim.api.nvim_create_user_command(
+				"Colorschemes",
+				function() require("telescope.builtin").colorscheme() end,
+				{ nargs = 0 }
+			)
+		end
+	}
+}
