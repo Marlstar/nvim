e6391e242c7cbf4b0af195d128e7c3486d34c450
Marley Reeves <marley@marlstar.com>
2025-01-17 20:25:58 +1000

    fix typo

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -38,7 +38,7 @@ return {
 			vim.api.nvim_create_autocmd("LspAttach", {
 				desc = "LSP actions",
 				callback = function(event)
-					local opts = { buffer = event.buf },
+					local opts = { buffer = event.buf }
 
 					vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
 					vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
