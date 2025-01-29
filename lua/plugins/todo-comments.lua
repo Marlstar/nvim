f7771798f2a729f467723b08df4d2f5ac6e13e63
Marley Reeves <marley@marlstar.com>
2025-01-29 15:23:59 +0000

    todo-comments plugin

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,24 @@
+return {
+	"folke/todo-comments.nvim",
+	event = "VeryLazy",
+	dependencies = "nvim-lua/plenary.nvim",
+
+	opts = {
+		highlight = {
+			multiline = false,
+			keyword = "wide_bg",
+		},
+		keywords = {
+			DONE = { icon = " ", color = "DONE", alt = {"FINISHED", "COMPLETE"}},
+			REVIEW = { icon = " ", color = "REVIEW", alt = {"CHECK"}},
+		},
+		colors = {
+			DONE = { "green", "DiffAdded", "#c3e88d" },
+			REVIEW = { "blue", "MiniIconsBlue", "#82aaff" }
+		}
+	},
+
+	keys = {
+		{ "<leader>ft", ":TodoTelescope<CR>", desc = "Todo-Comments Telescope", silent = true }
+	}
+}
