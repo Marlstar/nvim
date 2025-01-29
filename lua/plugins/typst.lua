9bc74c901ef0f5b8e41f8731ce5e841532fdd699
Marley Reeves <marley@marlstar.com>
2025-01-29 15:23:46 +0000

    auto-reset sw and tw when entering typst files as it changes for some reason

--- lua/plugins/typst.lua
+++ lua/plugins/typst.lua
@@ -12,6 +12,19 @@ return {
 			dependencies_bin = {
 				["tinymist"] = vim.fn.stdpath("data") .. "/mason/bin/tinymist"
 			}
-		}
+		},
+
+		config = function(_, opts)
+			vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+				pattern = { "*.typ" },
+				callback = function()
+					-- vim.cmd("set sw=4")
+					-- vim.cmd("set tw=4")
+					require("vimrc")
+				end,
+				desc = "Reset tw and sw when entering typst files"
+			})
+		end,
+
 	}
 }
