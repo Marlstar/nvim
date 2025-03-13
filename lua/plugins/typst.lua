a906f67aa9fcd32411d3102a602eaa4de6a89988
Marley Reeves <marley@marlstar.com>
2025-03-13 20:49:10 +1000

    typst: update things

--- lua/plugins/typst.lua
+++ lua/plugins/typst.lua
@@ -3,16 +3,6 @@ return {
 		"Marlstar/typst.vim",
 		ft = "typst",
 		lazy = false,
-		config = function()
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = { "*.typ" },
-				callback = function()
-					vim.api.nvim_buf_set_keymap(0, "n", "j", "gj", {silent=true})
-					vim.api.nvim_buf_set_keymap(0, "n", "k", "gk", {silent=true})
-				end,
-				desc = "Go down softwrapped lines in typst files"
-			})
-		end
 	},
 	{
 		"chomosuke/typst-preview.nvim",
@@ -24,9 +14,5 @@ return {
 			},
 			open_cmd = "firefox %s -P typst-preview --class typst-preview",
 		},
-
-		config = function(_, opts)
-			require("typst-preview").setup(opts)
-		end,
 	}
 }
