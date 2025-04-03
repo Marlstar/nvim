81cc723f6f3810784b035a85653180099350ad27
Marley Reeves <marley@marlstar.com>
2025-04-03 12:27:22 +1000

    neogit > fugitive

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,3 +1,14 @@
 return {
-	"tpope/vim-fugitive"
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		opts = {
+			graph_style = "kitty",
+			kind = "floating",
+		}
+	}
 }
