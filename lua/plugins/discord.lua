94b9023ff639018afaf88b2036b1390df23a7361
Marley Reeves <marley@marlstar.com>
2025-04-10 10:49:20 +1000

    update discord rich presence plugin from `andweeb/presence.nvim` to `IogaMaster/neocord`

--- lua/plugins/discord.lua
+++ lua/plugins/discord.lua
@@ -1,24 +1,25 @@
 return {
-	"andweeb/presence.nvim",
+	"IogaMaster/neocord",
 	opts = {
-		auto_update = true,
-		neovim_image_text = "neovim",
-		main_image = "file",
-		client_id = "793271441293967371",
-		log_level = nil,
-		debounce_timeout = 10,
-		enable_line_number = false,
-		buttons = true,
-		file_assets = {},
-		show_time = true,
+		logo                = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Neovim-mark.svg/1200px-Neovim-mark.svg.png",
+		logo_tooltip        = nil,                        -- nil or string
+		main_image          = "language",                 -- "language" or "logo"
+		client_id           = "1157438221865717891",      -- Use your own Discord application client id (not recommended)
+		log_level           = nil,
+		debounce_timeout    = 10,
+		blacklist           = {}, -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
+		file_assets         = {},
+		show_time           = true,
+		global_timer        = false, -- if set true, timer won't update when any event are triggered
 
 		-- Rich Presence text options
-		editing_text = "Editing %s",
-		file_explorer_text = "Browsing %s",
-		git_commit_text = "Committing changes",
+		editing_text        = "Editing %s",
+		file_explorer_text  = "Browsing %s",
+		git_commit_text     = "Committing changes",
 		plugin_manager_text = "Managing plugins",
-		reading_text = "Reading %s",
-		workspace_text = "Working on %s",
-		line_number_text = "Line %s out of %s",
+		reading_text        = "Reading %s",
+		workspace_text      = "Working on %s",
+		line_number_text    = "Line %s of %s",
+		terminal_text       = "Using Terminal",
 	}
 }
