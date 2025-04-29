a1fe53bef0a9af6d9d3e4da0f4838a8a40f31526
Marley Reeves <marley@marlstar.com>
2025-04-30 08:23:11 +1000

    reapply undercurl -> underline change when theme changes

--- lua/colour.lua
+++ lua/colour.lua
@@ -2,11 +2,21 @@
 vim.cmd("colorscheme tokyonight-moon")
 
 -- Replace undercurls with underlines
-local highlights = vim.api.nvim_get_hl(0, {})
-for name, opts in pairs(highlights) do
-	if opts.undercurl then
-		opts.undercurl = nil
-		opts.underline = true
-		vim.api.nvim_set_hl(0, name, opts)
+local function undercurl_to_underline()
+	local highlights = vim.api.nvim_get_hl(0, {})
+	for name, opts in pairs(highlights) do
+		if opts.undercurl then
+			opts.undercurl = nil
+			opts.underline = true
+			vim.api.nvim_set_hl(0, name, opts)
+		end
 	end
 end
+
+undercurl_to_underline()
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	callback = function()
+		vim.schedule(undercurl_to_underline)
+	end,
+})
