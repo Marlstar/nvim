02d3566482bf72d81147a0db9e11296e2f4bae9e
Marley Reeves <marley@marlstar.com>
2025-06-04 19:13:21 +1000

    mappings: fix system clipboard mappings to use visual mode as well

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,14 +1,13 @@
 local M = {}
 
-local function map(mode, key, map, opts)
+local function map(mode, key, map_, opts)
 	local options = { noremap = true, silent = true }
 	if opts then
 		options = vim.tbl_extend("force", options, opts)
 	end
-	vim.keymap.set(mode, key, map, options)
+	vim.keymap.set(mode, key, map_, options)
 end
 M.map = map
-
 
 
 function M.esc()
@@ -17,8 +16,7 @@ end
 
 
 function M.main()
-	map("n", "<leader>p", '"+p', { desc = "Paste from system clipboard" })
-	map("n", "<leader>y", '"+y', { desc = "Copy to system clipboard" })
+	map({"n","v"}, "<leader>p", '"+p', { desc = "Paste from system clipboard" })
+	map({"n","v"}, "<leader>y", '"+y', { desc = "Copy to system clipboard" })
 end
-
 return M
