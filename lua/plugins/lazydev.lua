7b7a9dfdf9c25dd8137c3a24d9033ca1679f4aeb
Marley Reeves <marley@marlstar.com>
2025-07-31 09:25:11 +1000

    chore: remove `nvim-cmp` entirely

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -9,14 +9,4 @@ return {
 			}
 		}
 	},
-	{ -- cmp completion
-		"hrsh7th/nvim-cmp",
-		opts = function(_, opts)
-			opts.sources = opts.sources or {}
-			table.insert(opts.sources, {
-				name = "lazydev",
-				group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-			})
-		end,
-	},
 }
