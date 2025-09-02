f529bc849bb9cb540583a63666f14aaccca276fc
Marley Reeves <marley@marlstar.com>
2025-09-03 09:16:57 +1000

    discord: move to `cord.nvim`

--- lua/plugins/discord.lua
+++ lua/plugins/discord.lua
@@ -1,25 +1,43 @@
 return {
-	"IogaMaster/neocord",
+	"vyfor/cord.nvim",
+	build = ":Cord update",
 	opts = {
-		logo                = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Neovim-mark.svg/1200px-Neovim-mark.svg.png",
-		logo_tooltip        = nil,                        -- nil or string
-		main_image          = "language",                 -- "language" or "logo"
-		client_id           = "1157438221865717891",      -- Use your own Discord application client id (not recommended)
-		log_level           = nil,
-		debounce_timeout    = 10,
-		blacklist           = {}, -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
-		file_assets         = {},
-		show_time           = true,
-		global_timer        = false, -- if set true, timer won't update when any event are triggered
-
-		-- Rich Presence text options
-		editing_text        = "Editing %s",
-		file_explorer_text  = "Browsing %s",
-		git_commit_text     = "Committing changes",
-		plugin_manager_text = "Managing plugins",
-		reading_text        = "Reading %s",
-		workspace_text      = "Working on %s",
-		line_number_text    = "Line %s of %s",
-		terminal_text       = "Using Terminal",
-	}
+		editor = {
+			tooltip = "neovim"
+		},
+		display = {
+			theme = "atom",
+			flavor = "accent",
+		},
+		text = {
+			file_browser = "Browsing ${name}",
+			plugin_manager = "Managing plugins",
+			lsp = "Configuring LSPs",
+			vcs = "Committing changes",
+			terminal = "Terminal"
+		},
+		variables = true,
+		buttons = {
+			{ -- Repository if available, github if not
+				label = function(opts) return opts.repo_url and "Repository" or "Github" end,
+				url = function(opts) return opts.repo_url or "https://github.com/Marlstar" end,
+			}
+		},
+		plugins = {
+			["cord.plugins.visibility"] = {
+				rules = {
+					blacklist = {},
+					whitelist = {},
+				},
+			},
+		},
+	},
+	-- Setting up custom filetypes so that cord can see them
+	init = function()
+		vim.filetype.add({
+			pattern = {
+				[".*/hypr/.*%.conf"] = "hyprlang",
+			}
+		})
+	end,
 }
